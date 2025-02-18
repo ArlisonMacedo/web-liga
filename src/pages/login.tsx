8993a96628d76b8d5fc7e131502ddba0628eb0a6
ArlisonMacedo <arlisonmacedo526@gmail.com>
2025-02-18 12:02:24 -0300

    creatae form login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,13 +9,20 @@ export function Login() {
         password: '',
     })
 
-    const handleChange = (e) => {
+    const handleChange = (e: any) => {
         const { name, value } = e.target
         setFormData({
             ...formData,
             [name]: value
         })
     }
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        console.log('Dados do formulário:', formData);
+        // Aqui você pode enviar os dados para um backend, por exemplo
+    };
+
     return (
         <div className="w-screen h-screen bg-white">
             <div className='flex justify-center items-center'>
@@ -23,17 +30,37 @@ export function Login() {
                 <h1 className='text-secunday font-black text-6xl items-center'>Campeonato Copa da BR</h1>
             </div>
 
-            <div className='flex flex-col w-[50%] items-center justify-center'>
-                <h1 className='text-secundary'>Entre com os Dados</h1>
-                <form>
-                    <label htmlFor="">CPF</label>
-                    <input
-                        type='text'
-                        id="cpf"
-                        name="cpf"
-                        value={FormData.cpf}
+            <div className='flex flex-col items-center justify-self-auto bg-gray-200 h-96 p-4'>
+                <h1 className='text-secundary font-black text-3xl'>Entre com os Dados</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <div className='flex items-center justify-center'>
 
-                    />
+                        <label htmlFor="cpf" className='text-secundary font-black text-xl mr-6'>CPF</label>
+                        <input
+                            placeholder='0000000000'
+                            className='p-3 border-secundary border rounded h-10 w-full focus:none'
+                            type='text'
+                            id="cpf"
+                            name="cpf"
+                            value={formData.cpf}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <br />
+                    <div className='flex justify-center items-center'>
+
+                        <label htmlFor="password" className='text-secundary font-black text-xl mr-6' >Senha</label>
+                        <input
+                            className='border-secundary border rounded h-10 p-3'
+                            placeholder='********************'
+                            type='password'
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </div>
                 </form>
             </div>
         </div>
